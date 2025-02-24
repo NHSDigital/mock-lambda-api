0807ab39565d99f7fc686ee11a07ee32bd0cb4ab
Ellen Umukoro <ellen.umukoro1@net.nhs>
2025-02-24 18:39:52 +0000

    DS-3316 Add example files in all folders

---
+++ src/lambdas/lambda1/handlers/exampleHandler.py
@@ -0,0 +1,1 @@
+# Files in this folder will be used to handle requests/responses sent to and from the Lambda API and the main functionalities of the lambda.
---
+++ src/lambdas/lambda1/utlilities/exampleUtilities.py
@@ -0,0 +1,1 @@
+# These files are for functions/tools that are usually used in multiple places in the project and help the build the main functionality.
